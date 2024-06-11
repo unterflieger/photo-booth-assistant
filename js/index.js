818b535d6aeee1ad06bdd308935235cbd7c98db9
unterflieger <54466452+unterflieger@users.noreply.github.com>
2024-06-11 22:35:52 +0200

    User License Agreement added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Check for the termsAccepted cookie
+    if (!getCookie("termsAccepted")) {
+        console.log("No cookie found, showing terms modal.");
+        showTermsModal();
+    } else {
+        console.log("Cookie found, terms already accepted.");
+        hideTermsModal();
+    }
+
     document.getElementById('confirmbutton').addEventListener('click', sendData);
+    document.getElementById('acceptTerms').addEventListener('click', acceptTerms);
 });
 
 function sendData() {
@@ -51,4 +61,44 @@ function clearTextBoxes() {
     document.getElementById('emailinput').value = '';
     document.getElementById('ConfirmEmailinput').value = '';
     document.getElementById('numberinput').value = '';
+}
+
+function showTermsModal() {
+    var termsModal = document.getElementById('termsModal');
+    termsModal.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+}
+
+function hideTermsModal() {
+    var termsModal = document.getElementById('termsModal');
+    termsModal.style.display = 'none';
+    document.body.style.overflow = 'auto';
+}
+
+function acceptTerms() {
+    setCookie("termsAccepted", "true", 30); // Set the cookie before hiding the modal
+    console.log("Setting cookie and hiding terms modal.");
+    hideTermsModal();
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    console.log(`Cookie set: ${name}=${value}; expires=${expires}`);
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
 }
